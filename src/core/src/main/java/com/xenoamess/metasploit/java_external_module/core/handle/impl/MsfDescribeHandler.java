e0eca79de768c483b8a09a6d20aa54fb55377817
XenoAmess <xenoamess@gmail.com>
2021-02-16 15:33:44 +0800

    to jsonNode.

--- src/core/src/main/java/com/xenoamess/metasploit/java_external_module/core/handle/impl/MsfDescribeHandler.java
+++ src/core/src/main/java/com/xenoamess/metasploit/java_external_module/core/handle/impl/MsfDescribeHandler.java
@@ -1,13 +1,13 @@
 package com.xenoamess.metasploit.java_external_module.core.handle.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.xenoamess.metasploit.java_external_module.core.constants.MsfPluginResourceUrlStrings;
 import com.xenoamess.metasploit.java_external_module.core.entities.MsfRequest;
-import com.xenoamess.metasploit.java_external_module.core.entities.response.MsfMetadata;
 import com.xenoamess.metasploit.java_external_module.core.entities.response.MsfResponse;
 import com.xenoamess.metasploit.java_external_module.core.enums.MsfMethodTypeEnum;
 import com.xenoamess.metasploit.java_external_module.core.handle.MsfCannotHandleException;
 import com.xenoamess.metasploit.java_external_module.core.handle.MsfRequestHandler;
 import com.xenoamess.metasploit.java_external_module.core.utils.JsonUtil;
-import com.xenoamess.metasploit.java_external_module.core.constants.MsfPluginResourceUrlStrings;
 import java.io.IOException;
 import java.io.InputStream;
 import org.jetbrains.annotations.NotNull;
@@ -16,12 +16,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static com.xenoamess.metasploit.java_external_module.core.enums.MsfMethodTypeEnum.DESCRIBE;
 
-public class MsfDescribeHandler implements MsfRequestHandler<MsfRequest, MsfResponse<MsfMetadata>> {
+public class MsfDescribeHandler implements MsfRequestHandler<MsfRequest, MsfResponse<JsonNode>> {
 
     private static final transient Logger LOGGER =
             LoggerFactory.getLogger(MsfDescribeHandler.class);
 
-    private MsfMetadata msfMetadata;
+    private JsonNode msfMetadata;
 
     public MsfDescribeHandler() {
         this.initMsfMetadata(MsfPluginResourceUrlStrings.METADATA);
@@ -36,7 +36,7 @@ public class MsfDescribeHandler implements MsfRequestHandler<MsfRequest, MsfResp
                 InputStream msfMetadataInputStream =
                         this.getClass().getClassLoader().getResourceAsStream(metadataFilePath);
         ) {
-            this.msfMetadata = JsonUtil.getObjectMapper().readValue(msfMetadataInputStream, MsfMetadata.class);
+            this.msfMetadata = JsonUtil.getObjectMapper().readValue(msfMetadataInputStream, JsonNode.class);
         } catch (IOException e) {
             LOGGER.error("cannot load msfMetadata:", e);
         }
@@ -49,17 +49,17 @@ public class MsfDescribeHandler implements MsfRequestHandler<MsfRequest, MsfResp
 
     @Nullable
     @Override
-    public MsfResponse<MsfMetadata> handle(@NotNull MsfRequest msfRequest) throws MsfCannotHandleException {
+    public MsfResponse<JsonNode> handle(@NotNull MsfRequest msfRequest) throws MsfCannotHandleException {
         return new MsfResponse<>(msfRequest.getJsonrpc(), msfRequest.getId(), this.msfMetadata);
     }
 
     //-----getters and setters
 
-    public MsfMetadata getMsfMetadata() {
+    public JsonNode getMsfMetadata() {
         return msfMetadata;
     }
 
-    public void setMsfMetadata(MsfMetadata msfMetadata) {
+    public void setMsfMetadata(JsonNode msfMetadata) {
         this.msfMetadata = msfMetadata;
     }
 }
